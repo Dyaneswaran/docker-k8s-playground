b7c2f87132545adf7f4b686eb4733062bd372715
Dyaneswaran <dyaneswaran123@gmail.com>
2026-04-01 17:22:42 -0400

    Edited

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Up Skilling with Docker & Kuber</h1>
+          <h1>Docker + Kubernetes Playground</h1>
           <p>
             Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
           </p>
