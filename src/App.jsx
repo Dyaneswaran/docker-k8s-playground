6c8f4f73ab4d32e48a623714a241b57ffa1b7a7e
Dyaneswaran <dyaneswaran123@gmail.com>
2026-04-02 22:26:54 -0400

    Retrigger for Auto Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -17,8 +17,10 @@ function App() {
         </div>
         <div>
           <h1>Docker + Kubernetes Playground</h1>
-          <p>So far we have buit and auto deployed a docker image using CI/CD</p>
-          <p>Next Step: Kuber?</p>
+          <p>
+            So far we have buit and auto deployed a docker image using CI/CD
+          </p>
+          <p>Next Step: K8S?</p>
         </div>
         <button
           className="counter"
